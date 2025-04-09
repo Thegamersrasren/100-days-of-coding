6168a9733b51e738e6a3521a19623ee4e4e1b095
Thegamersrasren <garenagbaire@gmail.com>
2025-04-09 15:43:37 +0100

    added

--- Calculator.py
+++ Calculator.py
@@ -7,7 +7,7 @@ def sub(n1, n2):
     return n1 - n2
 
 def div(n1, n2):
-    if n2 == 0:  # Prevent division by zero
+    if n2 == 0:  
         return "Error: Division by zero is not allowed."
     return n1 / n2
 
@@ -38,12 +38,12 @@ def calc():
         answer = calculator[equation](n1, n2)
         print(f"{n1} {equation} {n2} = {answer}")
 
-        choice = input("Do you wish to continue calculating with this result? (Y or N): ").lower()
+        choice = input("Do you wish to continue  (Y or N): ").lower()
 
         if choice == "n":
             done = True
             print("Goodbye")
-            os.system('cls')  # This clears the console in Windows
+            os.system('cls') 
         elif choice == "y":
             n1 = answer
         else:
