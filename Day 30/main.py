af8e4e94a1a0d68563fc8104dcdedcfd5a87fb75
Thegamersrasren <garenagbaire@gmail.com>
2025-05-20 14:37:32 +0100

    Changed it up

--- Day 30/main.py
+++
@@ -1,6 +0,0 @@
-height = float(input("height: "))
-weight = int(input("weight: "))
-if height  > 3:
-    raise ValueError("Human Height cannot be over 3 metres")
-bmi = weight/height ** 2
-print (bmi)
